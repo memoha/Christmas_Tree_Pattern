7b78be29622eda0c009c11c2db11cf60d11ad12e
memoha <122819675+memoha@users.noreply.github.com>
2024-01-03 15:17:42 -0500

    Create christmas_tree.py

---
+++ christmas_tree.py
@@ -0,0 +1,13 @@
+height = 10
+for i in range(1, height+1):
+ spaces = ' ' * (height-i)
+ stars = '*' * (2*i - 1)
+ print(spaces + stars)
+
+ space2 = ' ' * (height-i)
+ decor = '.' * (2*i-1)
+ print(space2 + decor)
+
+space3 = ' ' * (height-2)
+trunk = '|_|'
+print(space3 + trunk)
